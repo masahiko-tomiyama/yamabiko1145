0b0a656de62e006bf9e072af314ff6ff1a2963d1
tomiyama,masahiko <tomiyama@yamabiko1145.net>
2023-11-25 13:40:53 +0900

    First Commit

---
+++ yamabiko1145/Pages/Login.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace yamabiko1145.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost(string id)
+    {
+        Thread.Sleep(2000);             // 機器からのレスポンスを2秒待って
+        Response.Redirect("./Privacy"); // 再表示
+
+        return;
+    }
+}
+
+
